19cd5a95e1bbf9750e61a8c2f5da475582b42321
Matheus Clemente <matheus.xb@gmail.com>
2024-03-27 21:06:43 -0300

    Add resizing based on grid scale

--- module.js
+++ module.js
@@ -6,13 +6,7 @@ Hooks.once("i18nInit", () => {
 		config: true,
 		default: 24,
 		type: Number,
-		onChange: (n) => {
-			canvas.tokens?.placeables.forEach((token) => {
-				if (canvas.dimensions.size >= 200) n *= 7 / 6;
-				else if (canvas.dimensions.size < 50) n *= 5 / 6;
-				token.tooltip.style.fontSize = n;
-			});
-		},
+		onChange: resizeTooltips,
 	});
 
 	game.settings.register("elevation-module", "hover", {
@@ -35,36 +29,45 @@ Hooks.once("i18nInit", () => {
 			});
 		},
 	});
-});
 
-Hooks.on("canvasReady", () => {
-	const fontSize = game.settings.get("elevation-module", "fontSize");
-	const hover = game.settings.get("elevation-module", "hover");
-
-	canvas.tokens?.placeables.forEach((token) => {
-		if (canvas.dimensions.size >= 200) {
-			token.tooltip.style.fontSize = fontSize * (7 / 6);
-		} else if (canvas.dimensions.size < 50) {
-			token.tooltip.style.fontSize = fontSize * (5 / 6);
-		} else {
-			token.tooltip.style.fontSize = fontSize;
-		}
-		token.tooltip.alpha = hover;
+	game.settings.register("elevation-module", "scaleToGridSize", {
+		name: "ELEVATION_MODULE.scaleToGridSize.name",
+		hint: "ELEVATION_MODULE.scaleToGridSize.hint",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean,
+		onChange: resizeTooltips,
 	});
 });
+
+function resizeTooltips() {
+	const fontSize = game.settings.get("elevation-module", "fontSize");
+	const hover = game.settings.get("elevation-module", "hover");
+	const scaleToGridSize = game.settings.get("elevation-module", "scaleToGridSize");
+	const size = scaleToGridSize ? canvas.dimensions.size / 100 * fontSize : fontSize;
+	canvas.tokens?.placeables.forEach((token) => resizeToken(token, size, hover));
+}
+
+function resizeToken(token, size, hover) {
+	if (canvas.dimensions.size >= 200) {
+		token.tooltip.style.fontSize = size * (7 / 6);
+	} else if (canvas.dimensions.size < 50) {
+		token.tooltip.style.fontSize = size * (5 / 6);
+	} else {
+		token.tooltip.style.fontSize = size;
+	}
+	token.tooltip.alpha = hover;
+}
+
+Hooks.on("canvasReady", resizeTooltips);
 
 Hooks.on("drawToken", (token) => {
 	const fontSize = game.settings.get("elevation-module", "fontSize");
 	const hover = game.settings.get("elevation-module", "hover");
-
-	if (canvas.dimensions.size >= 200) {
-		token.tooltip.style.fontSize = fontSize * (7 / 6);
-	} else if (canvas.dimensions.size < 50) {
-		token.tooltip.style.fontSize = fontSize * (5 / 6);
-	} else {
-		token.tooltip.style.fontSize = fontSize;
-	}
-	token.tooltip.alpha = hover;
+	const scaleToGridSize = game.settings.get("elevation-module", "scaleToGridSize");
+	const size = scaleToGridSize ? canvas.dimensions.size / 100 * fontSize : fontSize;
+	resizeToken(token, size, hover);
 });
 
 Hooks.on("hoverToken", (token, hovered) => {
